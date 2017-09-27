1c5dbe910627ef5bfbaf3e99867205951b59a2e3
Brian French <fr3nch13@gmail.com>
2017-09-27 13:26:05 -0700

    Added the option to load, or not load the helpers.
    Allows the developer to make their own html helper and extend BootstrapUI's helper if needed.

--- src/View/UIViewTrait.php
+++ src/View/UIViewTrait.php
@@ -16,6 +16,9 @@ trait UIViewTrait
      *      - If not set or true will use the plugin's layout
      *      - If a layout name passed it will be used
      *      - If false do nothing (will keep your layout)
+     *  - `loadHelpers`
+     *      - If true, load the BootstrapUI helpers (default)
+     *      - If false, don't load them. This allows the developer to extend them without having to unload them first.
      *
      * @return void
      */
@@ -29,12 +32,19 @@ trait UIViewTrait
             $this->layout = $options['layout'];
         }
 
-        $this->loadHelper('Html', ['className' => 'BootstrapUI.Html']);
-        $this->loadHelper('Form', ['className' => 'BootstrapUI.Form']);
-        $this->loadHelper('Flash', ['className' => 'BootstrapUI.Flash']);
-        $this->loadHelper('Paginator', ['className' => 'BootstrapUI.Paginator']);
-        if (class_exists('\Cake\View\Helper\BreadcrumbsHelper')) {
-            $this->loadHelper('Breadcrumbs', ['className' => 'BootstrapUI.Breadcrumbs']);
+        $loadHelpers = true;
+        if(isset($options['loadHelpers']) and $options['loadHelpers'] === false) {
+            $loadHelpers = false;
+        }
+
+        if($loadHelpers === true) {
+            $this->loadHelper('Html', ['className' => 'BootstrapUI.Html']);
+            $this->loadHelper('Form', ['className' => 'BootstrapUI.Form']);
+            $this->loadHelper('Flash', ['className' => 'BootstrapUI.Flash']);
+            $this->loadHelper('Paginator', ['className' => 'BootstrapUI.Paginator']);
+            if (class_exists('\Cake\View\Helper\BreadcrumbsHelper')) {
+                $this->loadHelper('Breadcrumbs', ['className' => 'BootstrapUI.Breadcrumbs']);
+            }
         }
     }
 }
