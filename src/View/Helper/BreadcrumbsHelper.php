69b9a536fb6c40606c81187cd2858c433c547693
julianpollmann <julian.pollmann@gmx.de>
2018-01-23 12:50:23 +0100

    Fix BreadcrumbsHelper seperator

--- src/View/Helper/BreadcrumbsHelper.php
+++ src/View/Helper/BreadcrumbsHelper.php
@@ -13,6 +13,7 @@ class BreadcrumbsHelper extends CoreBreadcrumbsHelper
             'wrapper' => '<nav aria-label="breadcrumb"><ol{{attrs}}>{{content}}</ol></nav>',
             'item' => '<li class="breadcrumb-item"{{attrs}}><a href="{{url}}"{{innerAttrs}}>{{title}}</a></li>',
             'itemWithoutLink' => '<li class="breadcrumb-item active" aria-current="page"{{attrs}}><span{{innerAttrs}}>{{title}}</span></li>',
+            'separator' => ''
         ]
     ];
 
