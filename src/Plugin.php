a497c40a2850609f94f0a19ff395dee3fe03b4c0
Chris Hallgren <challgren@gmail.com>
2019-11-26 20:30:04 -0600

    Create Plugin.php

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BootstrapUI;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'BootstrapUI';
+}
