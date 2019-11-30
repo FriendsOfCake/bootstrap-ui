34844fb8f9de6988d510a8302c7d8a44bc51b52f
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-29 23:07:53 -0500

    feat: Create Plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+namespace BootstrapUI;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for BootstrapUI
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'BootstrapUI';
+}
