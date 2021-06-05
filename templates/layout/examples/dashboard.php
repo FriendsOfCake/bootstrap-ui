5a8385d6f0c537d3e3531f32af35df772017b077
ndm2 <ndm2@users.noreply.github.com>
2021-06-05 13:30:39 +0200

    Fix CS.

--- templates/layout/examples/dashboard.php
+++ templates/layout/examples/dashboard.php
@@ -15,7 +15,11 @@ $this->start('tb_body_start');
 ?>
 <body <?= $this->fetch('tb_body_attrs') ?>>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <?= $this->Html->link(Configure::read('App.title'), '/', ['class' => 'navbar-brand col-md-3 col-lg-2 me-0 px-3']) ?>
+        <?= $this->Html->link(
+            Configure::read('App.title'),
+            '/',
+            ['class' => 'navbar-brand col-md-3 col-lg-2 me-0 px-3']
+        ) ?>
         <button
             class="navbar-toggler position-absolute d-md-none collapsed" type="button"
             data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
@@ -40,7 +44,8 @@ $this->start('tb_body_start');
             </nav>
 
             <main role="main" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center
+                            pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2 page-header"><?= $this->request->getParam('controller'); ?></h1>
                 </div>
 <?php
