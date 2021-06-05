5cf060d5d1cba2cbdf2c22ec3b6780eb265c439c
ndm2 <ndm2@users.noreply.github.com>
2021-06-05 13:19:13 +0200

    Fix template layouts/styles.
    Adapts HTML and CSS according to the updated Bootstrap 5.0 examples.

--- templates/layout/examples/dashboard.php
+++ templates/layout/examples/dashboard.php
@@ -5,30 +5,44 @@
 use Cake\Core\Configure;
 
 $this->Html->css('BootstrapUI.dashboard', ['block' => true]);
-$this->prepend('tb_body_attrs', ' class="' . implode(' ', [$this->request->getParam('controller'), $this->request->getParam('action')]) . '" ');
+$this->prepend(
+    'tb_body_attrs',
+        ' class="' .
+        implode(' ', [$this->request->getParam('controller'), $this->request->getParam('action')]) .
+        '" '
+);
 $this->start('tb_body_start');
 ?>
 <body <?= $this->fetch('tb_body_attrs') ?>>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <?= $this->Html->link(Configure::read('App.title'), '/', ['class' => 'navbar-brand col-sm-3 col-md-2 mr-0']) ?>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <?= $this->Html->link(Configure::read('App.title'), '/', ['class' => 'navbar-brand col-md-3 col-lg-2 me-0 px-3']) ?>
+        <button
+            class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+            data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
+            aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="#">Sign out</a>
             </li>
         </ul>
-    </nav>
+    </header>
 
     <div class="container-fluid">
         <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" style="">
+                <div class="position-sticky pt-3">
                     <?= $this->fetch('tb_sidebar') ?>
                 </div>
             </nav>
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-                <h1 class="page-header"><?= $this->request->getParam('controller'); ?></h1>
+
+            <main role="main" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2 page-header"><?= $this->request->getParam('controller'); ?></h1>
+                </div>
 <?php
 /**
  * Default `flash` block.
@@ -46,5 +60,6 @@ $this->start('tb_body_end');
 echo '</body>';
 $this->end();
 
-$this->append('content', '</main></div></div>');
 echo $this->fetch('content');
+
+$this->append('footer', '</main></div></div>');
--- templates/layout/examples/signin.php
+++ templates/layout/examples/signin.php
@@ -3,7 +3,12 @@
  * @var \Cake\View\View $this
  */
 $this->Html->css('BootstrapUI.signin', ['block' => true]);
-$this->prepend('tb_body_attrs', ' class="' . implode(' ', [$this->request->getParam('controller'), $this->request->getParam('action')]) . '" ');
+$this->prepend(
+    'tb_body_attrs',
+    ' class="text-center ' .
+    implode(' ', [$this->request->getParam('controller'), $this->request->getParam('action')]) .
+    '" '
+);
 $this->start('tb_body_start');
 /**
  * Default `flash` block.
